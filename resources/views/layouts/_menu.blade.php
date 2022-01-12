3369905cdd0dc55f473f67b9b2673b7f51fd6094
anantsoftcomputin <anantsoftcomputing@gmail.com>
2022-01-12 18:30:51 +0530

    added new 03

--- resources/views/layouts/_menu.blade.php
+++ resources/views/layouts/_menu.blade.php
@@ -142,16 +142,15 @@
 </li>
 @endcan
 
-@can(["view-help"])
+
 <li class="menu menu-single">
-    <a target="_blank" href="{{ url('/admin/help') }}" data-active="false" class="menu-toggle">
+    <a target="_blank" href="{{ url('/admin/ApplicationStatus') }}" data-active="false" class="menu-toggle">
         <div class="base-menu">
             <div class="base-icons">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-help-circle"><circle cx="12" cy="12" r="10"></circle><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
             </div>
-            <span>Help</span>
+            <span>Status</span>
         </div>
     </a>
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
 </li>
-@endcan
