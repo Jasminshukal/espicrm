29302f8ae9e5af9dda8c5d6d040af592216b5505
anantsoftcomputin <anantsoftcomputing@gmail.com>
2022-01-25 15:35:53 +0530

    dashboarsd 04

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,26 +6,29 @@
 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
     <div class="row widget-statistic">
         <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12 layout-spacing">
-            <div class="widget widget-one_hybrid widget-followers">
-                <div class="widget-heading">
-                    <div class="w-title">
-                        <div class="w-icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+                <a href="{{ route('Enquires.index') }}">
+                <div class="widget widget-one_hybrid widget-followers">
+                    <div class="widget-heading">
+                        <div class="w-title">
+                            <div class="w-icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+                            </div>
+                            <div class="">
+                                <p class="w-value">{{ count(\App\Models\Enquiry::all()) }}</p>
+                                <h5 class="">Enquiry</h5>
+                            </div>
                         </div>
-                        <div class="">
-                            <p class="w-value">{{ count(\App\Models\Enquiry::all()) }}</p>
-                            <h5 class="">Enquiry</h5>
+                    </div>
+                    <div class="widget-content">
+                        <div class="w-chart">
+                            <div id="hybrid_followers"></div>
                         </div>
                     </div>
                 </div>
-                <div class="widget-content">
-                    <div class="w-chart">
-                        <div id="hybrid_followers"></div>
-                    </div>
-                </div>
-            </div>
+            </a>
         </div>
         <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12 layout-spacing">
+            <a href="{{ route('assessments.index') }}">
             <div class="widget widget-one_hybrid widget-referral">
                 <div class="widget-heading">
                     <div class="w-title">
@@ -45,26 +48,29 @@
                     </div>
                 </div>
             </div>
+            </a>
         </div>
         <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12 layout-spacing">
-            <div class="widget widget-one_hybrid widget-engagement">
-                <div class="widget-heading">
-                    <div class="w-title">
-                        <div class="w-icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>
+            <a href="{{ route('Application.index') }}">
+                <div class="widget widget-one_hybrid widget-engagement">
+                    <div class="widget-heading">
+                        <div class="w-title">
+                            <div class="w-icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>
+                            </div>
+                            <div class="">
+                                <p class="w-value">{{ count(\App\Models\Application::all()) }}</p>
+                                <h5 class="">Application</h5>
+                            </div>
                         </div>
-                        <div class="">
-                            <p class="w-value">{{ count(\App\Models\Application::all()) }}</p>
-                            <h5 class="">Application</h5>
+                    </div>
+                    <div class="widget-content">
+                        <div class="w-chart">
+                            <div id="hybrid_followers3"></div>
                         </div>
                     </div>
                 </div>
-                <div class="widget-content">
-                    <div class="w-chart">
-                        <div id="hybrid_followers3"></div>
-                    </div>
-                </div>
-            </div>
+            </a>
         </div>
     </div>
 </div>
