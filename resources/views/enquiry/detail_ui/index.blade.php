7d09e07facb90bc38adab1662345b5301aae018f
Jasmin shukla <mr.jasmin.shukal@gmail.com>
2022-02-07 12:34:08 +0530

    approve profile 02

--- resources/views/enquiry/detail_ui/index.blade.php
+++ resources/views/enquiry/detail_ui/index.blade.php
@@ -31,7 +31,10 @@
         <div class="widget-one">
             <div class="p-1">
                 <h5>
-                    Student Name : {{ $enquiry->name }} <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle text-success bs-tooltip" title="Approved Profile."><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>
+                    Student Name : {{ $enquiry->name }}
+                    @if ($enquiry->Details->is_conform==1)
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle text-success bs-tooltip" title="Approved Profile."><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>
+                    @endif
                 </h5>
                 <h5>
                     Email : {{ $enquiry->email }}
